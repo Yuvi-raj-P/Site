ffb5357869bac54ae7a93dac07cc3d1864a9883d
Yuvraj P <yuvilds@gmail.com>
2024-09-19 15:45:54 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', function() {
+    const firstSvg = document.querySelector('.svg-elem-1');
+    const PLayer = document.querySelector('.hidden.spacing');
+    const secondSvg = document.querySelector('.svg-elem-2');
+    const dotSvg = document.querySelector('.hidden.circle');
+    
+    firstSvg.classList.add('animate');
+    
+    setTimeout(() => {
+        dotSvg.classList.remove('hidden');
+        dotSvg.classList.add('fade-in');
+    }, 4000);
+
+    firstSvg.addEventListener('animationend', function() {
+        console.log('First animation ended');
+        
+        setTimeout(() => {
+            console.log('Adding animate class to second SVG');
+            secondSvg.classList.add('animate');
+            PLayer.classList.remove('hidden');
+        }, 50);
+    });
+});
